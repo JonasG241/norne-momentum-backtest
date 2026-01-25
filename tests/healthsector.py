89ec4383baea2ad5a8f283aaf54d23f4a4534015
John Ørbæk <johnoerbak@gmail.com>
2026-01-25 14:47:42 +0100

    startet på python

--- tests/healthsector.py
+++ tests/healthsector.py
@@ -1,3 +1,42 @@
+import pandas as pd
+
+
+
+
+datasets = [
+    "Arctic Bioscience AS Stock Price History.csv",
+    "Arcticzymes Tech Stock Price History.csv",
+    "Circio Holding Stock Price History.csv",
+    "Codelab Capital AS Stock Price History.csv",
+    "ContextVision AB Stock Price History.csv",
+    "Exact Therapeutics AS Stock Price History.csv",
+    "Gentian Diagnostics Stock Price History.csv",
+    "INIFY Laboratories Stock Price History.csv",
+    "Lifecare Stock Price History.csv",
+    "Lytix Biopharma AS Stock Price History.csv",
+    "Medistim Stock Price History.csv",
+    "Navamedic Stock Price History.csv",
+    "Nordhealth AS Stock Price History.csv",
+    "Nykode Therapeutics Stock Price History.csv",
+    "Observe Medical Stock Price History.csv",
+    "Omda AS Stock Price History.csv",
+    "Oncoinvent Stock Price History.csv",
+    "PCI Biotech Stock Price History.csv",
+    "Photocure Stock Price History.csv",
+    "SoftOx Solutions Stock Price History.csv",
+    "Thor Medical Stock Price History.csv",
+    "Vistin Pharma ASA Stock Price History.csv",
+    "Zelluna Stock Price History.csv",
+]
+
+
 
 
 start_date = 0 
+
+
+
+dt = pd.read_csv("norne-momentum-backtest/data/raw/Arctic Bioscience AS Stock Price History.csv")
+
+
+
